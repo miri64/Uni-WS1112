c51e2ddc236b423be2abc3e5c8154d2a7ee8dad6
Authmillenon <martin@ucsmail.de>
2011-10-28 02:09:49 +0200

    [HA]: I.3c Usage message

--- HA/u01/src/SortMain.java
+++ HA/u01/src/SortMain.java
@@ -1,8 +1,9 @@
-/**
- * @author Christian Cikryt
- */
 public class SortMain {
 	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("Usage: java -jar u01.jar length M");
+			System.exit(1);
+		}
 		int m = Integer.parseInt(args[0]);
 		int n = Integer.parseInt(args[1]);
 		SortTest test = new SortTest();
