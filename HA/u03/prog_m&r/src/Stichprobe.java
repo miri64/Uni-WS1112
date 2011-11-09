7277677800a78f67d6f7de1639206397b055c896
Authmillenon <authmill@datalove.me>
2011-11-09 22:53:32 +0100

    [HA]: size() == 0 durch laufzeitoptimaleres isEmpty() ersetzt

--- HA/u03/prog_m&r/src/Stichprobe.java
+++ HA/u03/prog_m&r/src/Stichprobe.java
@@ -13,10 +13,9 @@ public class Stichprobe implements Comparable<Stichprobe> {
 	}
 	
 	public long mittelwert() {
-		if (values.size() == 0) {
+		if (values.isEmpty()) {
 			return 0;
 		}
-		
 		long mittelwert = 0;
 		for (Long l : values) {
 			mittelwert += l.longValue();
@@ -26,7 +25,7 @@ public class Stichprobe implements Comparable<Stichprobe> {
 	
 	// Mittlere Absolute Abweichung als Streuung
 	public long maa() {
-		if (values.size() == 0) {
+		if (values.isEmpty()) {
 			return 0;
 		}
 		long maa = 0;
